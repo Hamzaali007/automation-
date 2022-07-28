3f82a655bb94ba7851c3336719da28df1d540b5c
Hamzaali007 <110109914+Hamzaali007@users.noreply.github.com>
2022-07-28 14:46:59 +0500

    Add files via upload

---
+++ automate notepad/main.py
@@ -0,0 +1,52 @@
+#author name: Syed Hamza Ali
+#python programming
+##########################################################################################
+import speech_recognition as sr
+import pyttsx3
+import os
+
+
+
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voices',voices[0].id)
+engine. setProperty("rate", 245)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("listening....")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+    try:
+        print("recognising....")
+        query = r.recognize_google(audio, language='eng-in')
+        print(f"user said:{query}\n")
+    except Exception as e:
+        print(e)
+        print('unable to recognise your voice')
+        return "None"
+    return query
+
+def notepad():
+    speak("i am ready to make your notes")
+    print("i am ready to make your notes")
+    speak("speak up your notes")
+    writes = takeCommand()
+    filename = "jarvis-note.txt"
+    with open(filename, "w") as file:
+        file.write(writes)
+    path_1 = "Enter your path" +str(filename)
+    os.startfile(path_1)
+
+
+if __name__=='__main__':
+    takeCommand()
+    notepad()
